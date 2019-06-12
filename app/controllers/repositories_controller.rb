49a9861d4b361ac09fecac1e31cb6fd016d8f8b2
Nicolas Marini <nmarini44@gmail.com>
2019-06-12 00:43:02 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,8 @@
 class RepositoriesController < ApplicationController
 
   def index
-    response = Faraday.get "https://api.github.com"
+    response = Faraday.get "https://api.github.com" do |req|
+      binding.pry
   end
 
 end
